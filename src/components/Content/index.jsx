7e7f83257305ba41b92a4868aefb8312d107e04b
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-03-05 07:55:13 -0300

    Added new styled animation from components

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -3,13 +3,14 @@ import { motion } from "framer-motion";
 export function Content({ title, children }) {
     return (
         <Container 
-            animate={{ x: [150, 0], opacity: 1, scale: 1 }}
+            animate={{ y: [150, 0], opacity: 1, scale: 1, filter:"blur(0)"}}
             transition={{
                 duration: 1.2,
                 delay: 0.1,
-                ease: [0.5, 0.71, 1, 1.2],
+                // ease: [0.5, 0.71, 1, 1.2],
+                type:"tween"
             }}
-            initial={{ opacity: .5, scale: .7}}
+            initial={{ opacity: .3, scale: .7, filter:"blur(12px)" }}
         >
             <h2>{ title }</h2>
             { children }
--- src/components/Tag/styles.js
+++ src/components/Tag/styles.js
@@ -7,9 +7,11 @@ export const Container = styled.span`
     border-radius: 7px;
     color: ${({theme, isActive}) => isActive ? theme.COLORS.BACKGROUND_700 : theme.COLORS.WHITE};
     background-color: ${({theme, isActive}) => isActive ? theme.COLORS.WHITE : theme.COLORS.BACKGROUND_700};
-    
+    transform: ease all .3s;
 
     &:hover{
         cursor: pointer;
+        background-color: ${({theme}) => theme.COLORS.WHITE};
+        color: ${({theme}) => theme.COLORS.BACKGROUND_700};
     }
 `;
