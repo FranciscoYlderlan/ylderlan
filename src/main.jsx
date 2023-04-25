9a51d57938f854b597d26420084a5f41f9e504df
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-04-24 21:46:56 -0300

    feat: Added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import GlobalStyle  from './styles/global.js';
 import theme from './styles/theme.js';
 import { Routes } from './routes/index.jsx';
 
+import { Analytics } from '@vercel/analytics/react';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
     <GlobalStyle/>
       <Routes/>
+      <Analytics/>
     </ThemeProvider>
   </React.StrictMode>,
 )
