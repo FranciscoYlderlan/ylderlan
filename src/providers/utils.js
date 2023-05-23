911cc152236e1d9f154efdb3eaea70e8ce0172db
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-05-23 15:31:31 -0300

    fix: Resolve error when added empty description

--- src/providers/utils.js
+++ src/providers/utils.js
@@ -1,84 +1,76 @@
-
 const dictThemes = [
-    'styled-components',
-    'localStorage',
-    'mobile first',
-    'tailwindcss',
-    'typescript',
-    'javascript',
-    '@keyframes',
-    'frontend',
-    'backend',
-    'express',
-    'fastify',
-    'android',
-    'sqlite',
-    'nodejs',
-    '@media',
-    'prisma',
-    'clamp',
-    'axios',
-    'react',
-    'knex',
-    'html',
-    'grid',
-    'git',
-    'css',
-    'dom',
-    'spa',
-    'es6',
-    'api',
-    'rem',
-    'sql',
+  "styled-components",
+  "localStorage",
+  "mobile first",
+  "tailwindcss",
+  "typescript",
+  "javascript",
+  "@keyframes",
+  "frontend",
+  "backend",
+  "express",
+  "fastify",
+  "android",
+  "sqlite",
+  "nodejs",
+  "@media",
+  "prisma",
+  "clamp",
+  "axios",
+  "react",
+  "knex",
+  "html",
+  "grid",
+  "git",
+  "css",
+  "dom",
+  "spa",
+  "es6",
+  "api",
+  "rem",
+  "sql",
 ];
 
-
-function sorted(value1, value2,type="incre"){
-    if (type == "incre"){
-        if(value1 < value2) 
-            return 1; 
-        if(value1 > value2)  
-            return -1; 
-        return 0;
-    }
-    if(value1 > value2) 
-        return 1; 
-    if(value1 < value2)  
-        return -1; 
+function sorted(value1, value2, type = "incre") {
+  if (type == "incre") {
+    if (value1 < value2) return 1;
+    if (value1 > value2) return -1;
     return 0;
+  }
+  if (value1 > value2) return 1;
+  if (value1 < value2) return -1;
+  return 0;
 }
 
+function tagsTitleGenerator({ name, description }) {
+  let tags = [];
+  if (!name) {
+    name = "no resources";
+  }
+  if (!description) {
+    description = "no resources";
+  }
+  dictThemes.map((theme) =>
+    description.toLowerCase().includes(theme)
+      ? (tags = [...tags, theme])
+      : false
+  );
 
-function tagsTitleGenerator({name,description}){
-    let tags = [];
-    
-    dictThemes.map(theme => 
-        description.toLowerCase().includes(theme) ? tags = [...tags, theme] : false
-    );
+  dictThemes.map((theme) =>
+    name.toLowerCase().includes(theme) ? (tags = [...tags, theme]) : false
+  );
 
-    dictThemes.map(theme => 
-        name.toLowerCase().includes(theme) ? tags = [...tags, theme] : false
-    );
-    
-    const uniqueTags = tags.filter((element, index) => {
-        return tags.indexOf(element) === index;
-    });
+  const uniqueTags = tags.filter((element, index) => {
+    return tags.indexOf(element) === index;
+  });
 
-    return uniqueTags
+  return uniqueTags;
 }
 
-
-function isUplouded(project){
-        
-    const {name ,homepage, has_pages} = project;
-    const existHomepage = (homepage && homepage !== "");
-    return (has_pages || existHomepage);
-    
+function isUplouded(project) {
+  const { name, homepage, has_pages } = project;
+  const existHomepage = homepage && homepage !== "";
+  return has_pages || existHomepage;
 }
 
-
-export {
-    sorted,
-    tagsTitleGenerator,
-    isUplouded,
-}
+export { sorted, tagsTitleGenerator, isUplouded };
--- src/services/Github.service.js
+++ src/services/Github.service.js
@@ -1,82 +1,81 @@
-import * as utils from "../providers/utils.js"
+import * as utils from "../providers/utils.js";
 
 export class GithubRepos {
-    
+  static async searchRepos(sort = "", direction = "") {
+    const endpoint = `https://api.github.com/users/franciscoylderlan/repos?sort=${sort}&direction=${direction}`;
+    const result = await fetch(endpoint, {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_USER_TOKEN}`,
+        "User-Agent": "Portfólio Ylderlan",
+        Accept: "application/vnd.github.v3+json",
+        "Content-Type": "application/json",
+      },
+    });
+    const repos_infos = await result.json();
+    let formatted_repos_infos = [];
+    Object.values(repos_infos).map(
+      ({ name, description, html_url, created_at, homepage, has_pages }) =>
+        (formatted_repos_infos = [
+          {
+            name,
+            description,
+            html_url,
+            created_at,
+            homepage,
+            has_pages,
+          },
+          ...formatted_repos_infos,
+        ])
+    );
+    return formatted_repos_infos;
+  }
 
-    static async searchRepos(sort='',direction='') {
-        const endpoint = `https://api.github.com/users/franciscoylderlan/repos?sort=${sort}&direction=${direction}`
-        const result = await fetch(endpoint,
-        {
-            headers:{
-                'Authorization': 'Bearer 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'
-            }
-        });
-        const repos_infos = await result.json();
-        let formatted_repos_infos = []; 
-        Object.values(repos_infos).map(
-            ({name, description, html_url,created_at, homepage, has_pages}) => (
-            formatted_repos_infos = [{
-                name,
-                description,
-                html_url,
-                created_at,
-                homepage,
-                has_pages
-            }, ... formatted_repos_infos]
-        ));
-        return formatted_repos_infos;
+  static async filterRepos(option = ["all"]) {
+    let sort = "";
+    let direction = "";
+
+    if (option.includes("new") || option.includes("old")) {
+      sort = "true";
+      if (option.includes("new")) {
+        direction = "asc";
+      } else {
+        direction = "desc";
+      }
     }
 
-    static async filterRepos(option = ["all"]){
-        
-        let sort = '';
-        let direction = '';
-        
-        if(option.includes("new") || option.includes("old")){
-            sort = 'true';
-            if(option.includes("new")){
-                direction='asc';
-            }else{
-                direction='desc';
-            }
-        }
-        
-        let aux = await GithubRepos.searchRepos(sort,direction);
+    let aux = await GithubRepos.searchRepos(sort, direction);
 
-        if(option.includes("all")) return aux;  
+    if (option.includes("all")) return aux;
 
-        if(option.includes("code"))
-            aux = aux.filter(repos => !utils.isUplouded(repos));
-        
-        if(option.includes("page"))
-            aux = aux.filter(repos => utils.isUplouded(repos));
-        
-        // if(option.includes("new"))
-        //     aux = aux.sort((repos1, repos2) => 
-        //     utils.sorted(Date.parse(repos1.created_at),Date.parse(repos2.created_at)));
-        
-        // if(option.includes("old"))
-        //     aux = aux.sort((repos1, repos2) => 
-        //     utils.sorted(Date.parse(repos1.created_at),Date.parse(repos2.created_at), "decre"));
-        
-        return aux
-          
+    if (option.includes("code"))
+      aux = aux.filter((repos) => !utils.isUplouded(repos));
+
+    if (option.includes("page"))
+      aux = aux.filter((repos) => utils.isUplouded(repos));
+
+    // if(option.includes("new"))
+    //     aux = aux.sort((repos1, repos2) =>
+    //     utils.sorted(Date.parse(repos1.created_at),Date.parse(repos2.created_at)));
+
+    // if(option.includes("old"))
+    //     aux = aux.sort((repos1, repos2) =>
+    //     utils.sorted(Date.parse(repos1.created_at),Date.parse(repos2.created_at), "decre"));
+
+    return aux;
+  }
+
+  static getPageURL(repository) {
+    const { name, html_url, homepage, has_pages } = repository;
+    const existHomePage = homepage && homepage !== "";
+
+    if (has_pages || existHomePage) {
+      if (existHomePage)
+        return homepage.includes("https://")
+          ? homepage
+          : `https://${homepage}/`;
+
+      return `https://franciscoylderlan.github.io/${name}/`;
     }
-
-    static getPageURL(repository){
-        const {name, html_url, homepage, has_pages} = repository;     
-        const existHomePage = (homepage && homepage !== "");
- 
-        if(has_pages || existHomePage){
-            
-            if(existHomePage) return homepage.includes("https://")? homepage :`https://${homepage}/`; 
-            
-            return `https://franciscoylderlan.github.io/${name}/`;
-        }
-        return html_url;
-    }
-    
-
+    return html_url;
+  }
 }
-
-
