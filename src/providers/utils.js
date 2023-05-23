f44eb703f6c032eba234bd1e4f7006ec31ad08b4
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-05-23 19:13:07 -0300

    fix: Added .env on gitignore

--- src/providers/utils.js
+++ src/providers/utils.js
@@ -35,6 +35,7 @@ function sorted(value1, value2, type = 'incre') {
     if (type == 'incre') {
         if (value1 < value2) return 1;
         if (value1 > value2) return -1;
+
         return 0;
     }
     if (value1 > value2) return 1;
